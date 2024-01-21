09fd804c8080f3ceec9fbed333b476672eeebd29
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-01-21 12:44:33 +0530

    EP 3 Completed

--- Code/App.js
+++ Code/App.js
@@ -11,16 +11,16 @@
 
 //$ 9) How to create nested structure inside React (using array of children in *  [] *  )
 
-    /* 
-
+    
+/* 
      <div id="parent">
         <div id="child">
             <h1>I'm h1 tag</h1>
             <h1>I'm h2 tag</h1>
         </div>
      </div>
-
-    ReactElement(Object) => HTML that browser understands */
+ */
+    // ReactElement(Object) => HTML that browser understands
 
     // const parent = React.createElement(
     //     "div",
@@ -74,10 +74,12 @@ import ReactDOM from "react-dom"
 //$ 31) BABEL also transpile the code for older versions of browsers so that they can also understand and render the site
 
 //$ 32) className="head" is converted into normal class in browser
-    // const jsxHeading = (
-    // <h1 id="heading" className="head">
-    //     HI I'M JSX HEADING
-    // </h1>);
+/* 
+    const jsxHeading = (
+    <h1 id="heading" className="head">
+        HI I'M JSX HEADING
+    </h1>);
+*/
 //$ 33) if u write jsx in multiple lines then you have to put the code in bracket it's mandatory
 
 //$ ________________________________________________________________________________________________________________________________________________________________________________________________________
@@ -98,30 +100,30 @@ import ReactDOM from "react-dom"
         //$ 4. Short hand syntax: no need to write return and the brackets "()" if the code is one liner as shown below :-
     
     // const fn = () => true;
-        //& or
+        //~ or
     // const fn = () =>(
     //     return true;
     // );
-        //& both are same
+        //~ both are same
 
 
     // const HeadingComponent = () => (
     //     return <h1>Namaste react Functional Component</h1>;
     // );
-        //& or 
+        //~ or 
     // const HeadingComponent = () => <h1>Namaste react Functional Component</h1>;
-        //& both are same
+        //~ both are same
 
 
-        // Sometimes we write return and some times we don't for EG:-
+    //^ Sometimes we write return and some times we don't for EG:-
     // const HeadingComponent = () => (
     //     return <h1> Namaste react Functional Component</h1>;
     // );
-        //& or 
+        //~ or 
     // const HeadingComponent = () => (
     //     <h1> Namaste react Functional Component</h1>;
     // );
-        //& both are same
+        //~ both are same
 
 
 //$ 35) How to enter this is in root.render or how to render it on webpage
@@ -130,39 +132,105 @@ import ReactDOM from "react-dom"
 
 //$ 36) Rendering a component inside heading component
     
-    const Title = () =>(
+
+   const Title = () =>(
     <div className="div">
-        <h1>Title Component</h1>
+        <h1>REACT Title Component</h1>
     </div>
     );
-
-    const HeadingComponent = () => (
-        <div id="container">
-            <Title/>
-            <h1> Namaste react Functional Component</h1>;
-        </div>
-        );
+    
+    
+    //  const HeadingComponent = () => (
+    //     <div id="container">
+    //         <Title/>       {/* OR <Title></Title> both are same */}
+    //     {/*YOU CAN WRITE ANY NUMBER OF TIMES THERE IS NO LIMIT TO IT */}
+    //            <Title/>
+    //            <Title></Title> 
+    //            <Title/>
+    //         <h1> Namaste react Functional Component</h1>;
+    //     </div>
+    //     ); 
+   
+   
 
         //$ 36.1) It means all the code of title will come in HeadingComponent
+    /* 
+        const HeadingComponent = () => (
+            <div id="container">
+                <div className="div">
+                <h1>Title Component</h1>
+            </div> 
+                <h1> Namaste react Functional Component</h1>;
+            </div>
+            );
+    */
+//$ 37) can we write components without using arrow function.
+//&     ==> YES YOU CAN.. YOU JUST HAVE TO REMOVE THE ARROW AND HAVE TO WRITE RETURN and mention function
+//$     ==> USE ARROW FUNCTION ONLY IT'S USED MOSTLY IN INDUSTRIES
+    /* 
+        const Title = function() {
+            return(
+            <div className="div">
+                <h1>Title Component</h1>
+            </div>
+            )
+        } 
+    */
 
+//& 38) ANYWHERE IN JSX PARENTHESIS (); YOU CAN WRITE ANY JS CODE INSIDE IT BY USING {} INSIDE IT
+
+    // const HeadingComponent = () => (
+    //&     YOU CAN WRITE ANY JS CODE INSIDE THIS BY USING {} INSIDE IT
+    //     );
+
+//* JS
+//& Title Above is just a JS variable we can also add title to it using {}
+
+  /*   
+        const TitleInJS = (
+            <div className="div">
+                <h1>Title In JS Component</h1>
+            </div>
+            );
+        const SpanElem = <span>React Element</span>
+        const number = 1000; 
+    */
+//* JS
+    
         // const HeadingComponent = () => (
         //     <div id="container">
-        //         <div className="div">
-        //         <h1>Title Component</h1>
-        //     </div> 
-        //         <h1> Namaste react Functional Component</h1>;
+        //         <h1>{number}</h1>
+        //         <h1>{number+100}</h1>
+        //         <h3>{TitleInJS}</h3>
+        // &         {/* CALLING REACT FUNCTION INSIDE JSX USING {ReactFUN()} for eg:-{Title()} */}
+        // ^         {/* these three things below are one of the same things */}
+        // ^         <h4>{Title()}</h4>
+        // ^         <Title/>
+        // ^         <Title></Title> 
+        //         <h1>{console.log("EVEN CONSOLE.LOG WILL ALSO WORK IN HERE BUT IT HAS TO BE IN {}")}</h1>
+        //         <h1>{SpanElem} Namaste react Functional Component</h1>
         //     </div>
-        //     );
+        //     ); 
+   
+//$ 39) Threats while taking data from an API
+// API can send malicious data to the browser and can get access to the all the cookies, data, session times & can also read local storage etc. of the user  
+// JSX helps in countering this problem.It takes care of these INJECTION ATTACKS.
+// JSX will escape the data if it is providing malicious data.
+// JSX don't runs blindly it will sanitize the data and then it will be provided to you
 
-//$ 37) can we write components without using arrow function.
-//     ==> YES YOU CAN YOU JUST HAVE TO REMOVE THE ARROW AND HAVE TO WRITE RETURN IF YOU WANT TO REMOVE ARROW 
+// Suppose getting data from an API
+// const data = api.getData();
 
-// just
-
-
-
-
-
+    // JSX WILL SANITIZE ANY PEACE OF CODE WRITTEN INSIDE THESE {}.
+    /* 
+            const HeadingComponent = () => (
+                <div id="container">
+                    <h1>{SpanElem} Namaste react Functional Component</h1>
+                    {data}
+        & JSX WILL SANITIZE ANY PEACE OF CODE WRITTEN INSIDE THESE {}.
+                    {Title}
+                </div>
+    */
 
 
 
