3eb0f5561f01330ce3555af057aecbb0fd566073
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-01-24 12:57:15 +0530

    EP#4 JSON Obj

--- SwiggyClone/App.js
+++ SwiggyClone/App.js
@@ -31,7 +31,19 @@ Footer
         </div>
     )
    }
+//~ 42) a. Creating a body container to provide dynamically data inside body 
+const resObj ={
+    type:"restaurant",
+    data: {
+        name:"KFC",
+        cuisines: ["Burger","Biryani", "American", "Chinese"],
+        avgRating: ["4"],
+        costForTwo:40000, // IN paise
+        deliveryTime: "36mins"
 
+    },
+   };
+//~ 42) b. Updating Body taking data from resObj
    const Body = () =>{
     return(
         <div className="body">
@@ -39,13 +51,14 @@ Footer
             <div className="ResContainer">
              {/* <div className="resCard"> RESTAURANT CARD WE WILL CREATE A SEPARATE COMPONENT FOR IT BCZ WE WILL BE RE-USING IT AGAIN AND AGAIN </div>  */}
                {/* resName =... cuisine=... are the props which are passed along with it */}
-                <ResCard resName="Meghana Foods"/>
-                <ResCard resName="Bawal Foods"/>
-                <ResCard resName="Burger King" cuisine="Burger, Fires"/>
-                <ResCard resName="Northen Ley" cuisine="Briyani, SouthIndian"/>
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
+                {/* <ResCard resData="Meghana Foods"/>
+                <ResCard resData="Northen Ley" cuisine="Briyani, SouthIndian"/> */}
+                {/* 42) c. <ResCard resName="Burger King" cuisine="Burger, Fires"/>
+                Lets not define data like this we will create a json object from where we will take the data in our body container */}
+                <ResCard resData={resObj}/>
+                {/* 42) d. data from the resObj in the form of props is provided to ResCard component */}
+                {/* 43) e. == IMP == from now onward resData is props for ResCard*/}
+                
             </div>
         </div>
     )
@@ -62,22 +75,23 @@ Footer
 
 //&  Props can be destructured it means instead of passing props to ResCard you can directly pass the arguments like this const ==> ResCard = ({resName, cuisine}) <== shown below:- 
 //  This is known as destructuring on the fly. Destructing is not part of react it is a concept of JS
-const ResCard = ({resName, cuisine}) =>{
-    console.log(resName, cuisine);
-    return (
-        <div className="ResCard">
-            <img className="ResLogo" src="https://previews.123rf.com/images/miracel123/miracel1231801/miracel123180100795/94312687-food-delivery-logo.jpg" alt=""/>
-            {/* <h3>DamFoods</h3> */}
-            {/* <h3>{props.resName}</h3>   */}
-            <h3>{resName}</h3>
-            {/* we can directly use resName no need to write like props.resName */}
-            <h4>{cuisine}</h4>
-            <h4>* 4.4</h4>
-            <h4>30mins</h4>
-            <h4>400Rs</h4>
-        </div>
-    )
-   }
+// const ResCard = ({resName, cuisine}) =>{
+//     console.log(resName, cuisine);
+//     return (
+//         <div className="ResCard">
+//             <img className="ResLogo" src="https://previews.123rf.com/images/miracel123/miracel1231801/miracel123180100795/94312687-food-delivery-logo.jpg" alt=""/>
+//             {/* <h3>DamFoods</h3> */}
+//             {/* <h3>{props.resName}</h3>   */}
+//             <h3>{resName}</h3>
+//             {/* we can directly use resName no need to write like props.resName */}
+//             <h4>{cuisine}</h4>
+//             <h4>* 4.4</h4>
+//             <h4>30mins</h4>
+//             <h4>400Rs</h4>
+//         </div>
+//     )
+//    }
+//^ OR Both are same
 // const ResCard = (props) =>{
 //     console.log(props);
 //     return (
@@ -93,6 +107,38 @@ const ResCard = ({resName, cuisine}) =>{
 //     )
 //    }
 
+//^  41) All This data comes in the form of API in JSON format 
+//     -> it stores the data which has to be loaded in site
+//     -> The data can vary according to city, area etc.
+//?    -> How to check Data of live sites like swiggy => Console -> Network -> Fetch/HMR -> preview
+//^ 41) CONFIG DRIVEN UI - It means UI is driven by Config
+//  What is Config? Data in the form of API is config
+//^ The Crousel which shows offers data can be different in different locations and there can be no data or offers for some locations. All this data is stored statewise or location wise So, when there's a change in location the data in the frontend changes.
+//  It is a very imp concept used in almost every website like amazon,uber etc. (Frontend System Design)
+//  FOR EG:- You want to show different offers with different BGcolor in different areas. Then we have to send data in the JSON format as a config and this config will help us to drive the ui or change the ui this is known as config driven UI.
+
+
+//~ 42) e.  We will take the data in the form of props inside ResCard from body which is taking data from resObj (an API,or a JSON object)
+//~ 42) f.  We are taking resData in brackets bcz we are taking an array of a data stored in resObj
+      const ResCard = ({resData}) =>{
+console.log(resData);
+    return (
+        <div className="ResCard">
+            <img className="ResLogo" src="https://previews.123rf.com/images/miracel123/miracel1231801/miracel123180100795/94312687-food-delivery-logo.jpg" alt=""/>
+            {/* <h3>DamFoods</h3> */}
+            {/* <h3>{props.resName}</h3> */}
+            <h3>{resData.data.name}</h3>
+            <h4>{resData.data.cuisines.join(", ")}</h4>
+            {/* Changing cuisines to coma(,) separated values.
+            from this BurgerBiryaniAmericanChinese to this Burger, Biryani, American, Chinese using array join(", ") in JS */}
+            <h4>{resData.data.avgRating}</h4>
+            <h4>{resData.data.deliveryTime}</h4>
+            <h4>{resData.data.costForTwo/100}</h4> 
+            {/* converting costForTwo in ruppes */}
+        </div>
+    )
+   }
+
    const AppLayout = () => {
     return (
     <div className="app">
@@ -101,5 +147,7 @@ const ResCard = ({resName, cuisine}) =>{
     </div>
         )
    }
+
    const root = ReactDOM.createRoot(document.getElementById("root"));
    root.render(<AppLayout/>);
+
