6401c06c2b4ec7e39f8660442c0e6cae1c0527c0
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-02-23 13:52:34 +0530

    onSubmit

--- React Notes(YT)/Handling Events in ReactJs/src/onSubmit/onSubmit.jsx
+++ React Notes(YT)/Handling Events in ReactJs/src/onSubmit/onSubmit.jsx
@@ -1,4 +1,4 @@
-//  $ 8.3) onSubmit 
+//  $ 8.3) onSubmit  (NET NINJA)
 // -> The onSubmit event handler in React is a fundamental part of working with forms. 
 // -> It allows you to capture form submissions and control how your application reacts to them.
 
@@ -7,20 +7,51 @@
 // -> 2. within submit handler we prevent page refresh
 // -> 3. implement post request with fetch api to send post data to designated api
 
+
+
+
 import React, { useState } from 'react'
+import "./onSubmit.css"
 
-const [title, setTitle]=useState();
-const [body, setbody]= useState();
-const [author, setauthor]= useState();
+const onSubmit = () => {
+const [title, setTitle]=useState("");
+const [body, setBody]= useState("");
+const [author, setAuthor]= useState("Aman");
 
-const onSubmitHook = () => {
+// USING ONSUBMIT
+//&  Q: why we use e.preventDefault(). or OnSubmit 
+//&  A: It is used to prevent the default function which refreshes the  page when clicked on submit.
+//&     To prevent page from loading when we click on submit button.
+ 
+const handleSubmit = (e) => {
+ e.preventDefault();
+//--> Creating a object (blog) to store response 
+  const blog = {title, body, author};
+//--> Now we can send a post request to a particular endPoint so that we can add our object created by user in our database.
+  console.log(blog);
+}
+  
   return (
     <div className='onSubmitContainer'>
+      <form onSubmit={handleSubmit}>
+
       <h1>Add a New Blog</h1>
-      <h2>Blog Title:</h2>
-      <input type="text" value={title}  />
+      <label>Blog Title:</label>
+      <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} />
+      
+      <label> Blog Body:</label>
+      <textarea type="text" value={body} onChange={(e)=>{setBody(e.target.value)}} />
+
+      <label>Blog Author:</label>
+      <select type="text" value={author} onChange={(e)=>{setAuthor(e.target.value)}} >
+        <option value="Aman">Aman</option>
+        <option value="Surya">Surya</option>
+      </select> 
+      <button>Add Blog</button>
+      
+      </form>
     </div>
   )
 }
 
-export default onSubmitHook
+export default onSubmit
