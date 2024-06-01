287ff70535ec338ac8cbdb38a9d335df7db6520c
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-06-01 09:07:04 +0530

    Folder Restructuring

---
+++ 🙏React EP 7.0 - ( Second Half )/src/components/Shimmer/Shimmer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Shimmer.css'
+const Shimmer = () => {
+  return (
+    <div className='shimmerContainer'>
+        <div className="shimmerCard">--------</div>
+        <div className="shimmerCard">--------</div>
+        <div className="shimmerCard">--------</div>
+        <div className="shimmerCard">--------</div>
+        <div className="shimmerCard">--------</div>
+        <div className="shimmerCard">--------</div>
+        <div className="shimmerCard">--------</div>
+        <div className="shimmerCard">--------</div>
+        <div className="shimmerCard">--------</div>
+    </div>
+  )
+}
+
+export default Shimmer
---
+++ 🙏React EP 7.0 - ( Second Half )/src/utils/constant.js
@@ -0,0 +1,6 @@
+// -> 6.1.4) Changing the CDN_URL from  
+// CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
+// To
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+
+export const LOGO_URL = "https://previews.123rf.com/images/miracel123/miracel1231801/miracel123180100795/94312687-food-delivery-logo.jpg";
