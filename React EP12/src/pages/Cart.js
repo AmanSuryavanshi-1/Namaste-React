a0e6e858a31d94d5c1ee80959d1895720f494121
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-07-24 15:58:17 +0530

    Created cart component

--- React EP12/src/pages/Cart.js
+++ React EP12/src/pages/Cart.js
@@ -1,33 +1,28 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import ItemList from '../components/ItemList';
+import { clearCart } from '../utils/cartSlice';
 
 const Cart = () => {
+  const cartItems = useSelector((store)=> store.cart.items);
+
+  const dispatch = useDispatch();
+  const handleClearCart = () =>{
+    dispatch(clearCart());
+  } 
+  
   return (
-    <div><div className="relative flex items-center justify-center min-h-screen overflow-hidden bg-primary-bgColor text-primary-white">
-    <div className="absolute inset-0 opacity-50 bg-gradient-to-br from-primary-bgColor "></div>
-    <div className="relative z-10 max-w-2xl p-10 mx-auto text-center transform -translate-y-12 border shadow-xl rounded-xl bg-primary-grey/30 backdrop-blur-md border-primary-white/10">
-      <h1 className="mb-6 font-serif text-5xl font-bold tracking-wide text-primary-yellow">
-        Cart Feature Coming Soon!
-      </h1>
-      <p className="mb-8 font-sans text-xl leading-relaxed text-primary-light">
-        We're Working on it. Get ready for a smarter food ordering experience!
-      </p>
-      <div className="inline-block px-8 py-4 font-sans text-lg font-semibold transition-all duration-300 ease-in-out transform rounded-full hover:scale-105 text-primary-dark bg-primary-yellow hover:bg-primary-light hover:text-primary-dark">
-        Stay Tuned
-      </div>
-    </div>
-    {[...Array(20)].map((_, i) => (
-      <div 
-        key={i}
-        className="absolute w-1 h-1 rounded-full bg-primary-yellow animate-ping"
-        style={{
-          left: `${Math.random() * 100}%`,
-          top: `${Math.random() * 100}%`,
-          animationDuration: `${Math.random() * 3 + 1}s`,
-          animationDelay: `${Math.random() * 2}s`,
-        }}
-      ></div>
-    ))}
-  </div><h1>Cart</h1></div>
+   <div>
+    <h1 className='text-3xl text-primary-light'>CART</h1>
+    {/* Displaying cart items */}
+    {cartItems.length === 0 && <h1 className='text-3xl text-primary-light'>YOUR CART IS EMPTY. ADD ITEMS TO YOUR CART</h1>}
+        <ItemList itemCardsData={cartItems}/>
+        {/* Clearing cart */}
+          <button className='bg-primary-yellow' 
+            onClick = {handleClearCart}
+            >Clear Cart
+          </button>
+   </div>
   )
 }
 
